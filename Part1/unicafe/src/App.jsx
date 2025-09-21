99958cf203be4636a231c291125bc030ac8e5555
Parham-F <parham.parvizfarzam@gmail.com>
2025-09-20 18:15:39 -0600

    Unicafe Step3 Commit

--- Part1/unicafe/src/App.jsx
+++ Part1/unicafe/src/App.jsx
@@ -1,50 +1,12 @@
 import { useState } from 'react'
 
-const App = () => {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  const [total, setTotal] = useState(0)
-
-  const increaseGood = () => {
-    const updatedGood = good + 1
-    setGood(updatedGood)
-    setTotal(updatedGood + neutral + bad)
-  }
-  const increaseNeutral = () => {
-    const updatedNeutral = neutral + 1
-    setNeutral(updatedNeutral)
-    setTotal(good + updatedNeutral + bad)
-  }
-  const increaseBad = () => {
-    const updatedBad = bad + 1
-    setBad(updatedBad)
-    setTotal(good + neutral + updatedBad)
-  }
-
+const Statistics = ({good, neutral, bad, total}) => {
   return (
     <div>
-      <h1>Give Feedback</h1>
-
-      <Button
-        onClick={increaseGood}
-        text='Good'
-      />
-      <Button
-        onClick={increaseNeutral}
-        text='Neutral'
-      />     
-      <Button
-        onClick={increaseBad}
-        text='Bad'
-      />
-
       <h1>Statistics</h1>
-      
       <Display label='Good ' counter={good}/>
       <Display label='Neutral ' counter={neutral}/>
       <Display label='Bad ' counter={bad}/>
-
       <p>All {total}</p>
       <Average good={good} bad={bad} total={total}/>
       <Positiveness good={good} total={total}/>
@@ -91,6 +53,41 @@ const Positiveness = ({good, total}) => {
     <p>
       Positive {res}%
     </p>
+  )
+}
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+
+  const increaseGood = () => {
+    const updatedGood = good + 1
+    setGood(updatedGood)
+    setTotal(updatedGood + neutral + bad)
+  }
+  const increaseNeutral = () => {
+    const updatedNeutral = neutral + 1
+    setNeutral(updatedNeutral)
+    setTotal(good + updatedNeutral + bad)
+  }
+  const increaseBad = () => {
+    const updatedBad = bad + 1
+    setBad(updatedBad)
+    setTotal(good + neutral + updatedBad)
+  }
+
+  return (
+    <div>
+      <h1>Give Feedback</h1>
+
+      <Button onClick={increaseGood} text='Good'/>
+      <Button onClick={increaseNeutral} text='Neutral'/>     
+      <Button onClick={increaseBad} text='Bad'/>
+      
+      <Statistics good={good} neutral={neutral} bad={bad} total={total} />
+    </div>
   )
 }
 
