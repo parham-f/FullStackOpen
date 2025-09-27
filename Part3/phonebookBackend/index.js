d175245a6fc2dcdc268fa8d7625f96d7e35989b3
Parham-F <parham.parvizfarzam@gmail.com>
2025-09-27 00:13:13 -0600

    PhoneBookBackend Step2 Commit

--- Part3/phonebookBackend/index.js
+++ Part3/phonebookBackend/index.js
@@ -26,10 +26,17 @@ let persons = [
 ]
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+    response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+    response.send(
+        `<p>Phonebook has info for ${persons.length} people</p>
+        <p>${Date()}</p>`
+    )
 })
 
 const PORT = 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+    console.log(`Server running on port ${PORT}`)
 })
