b2ddc916ddcbadcf54ef8bf0c02adc2677a62de4
Parham-F <parham.parvizfarzam@gmail.com>
2025-09-27 01:19:21 -0600

    PhoneBookBackend Step8 Commit

--- Part3/phonebookBackend/index.js
+++ Part3/phonebookBackend/index.js
@@ -3,7 +3,8 @@ const app = express()
 app.use(express.json())
 
 const morgan = require('morgan')
-app.use(morgan('tiny'))
+morgan.token('body', (req) => JSON.stringify(req.body))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
     { 
