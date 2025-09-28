280f917cbd7ca1c85856e01b6285cd963dfa3ed1
Parham-F <parham.parvizfarzam@gmail.com>
2025-09-28 01:27:56 -0600

    PhoneBookBackend Step10/11 Commit

--- Part3/phonebookBackend/index.js
+++ Part3/phonebookBackend/index.js
@@ -2,8 +2,7 @@ const express = require('express')
 const app = express()
 app.use(express.json())
 
-const cors = require('cors')
-app.use(cors())
+app.use(express.static('dist'))
 
 const morgan = require('morgan')
 morgan.token('body', (req) => JSON.stringify(req.body))
