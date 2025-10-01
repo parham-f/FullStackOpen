cba2cd02e3b11b2052bdaad00d144c264249195b
Parham-F <parham.parvizfarzam@gmail.com>
2025-10-01 16:36:13 -0600

    4.9: Blog List Tests, step 2

--- Part4/blogList/tests/blog_api.test.js
+++ Part4/blogList/tests/blog_api.test.js
@@ -13,10 +13,24 @@ beforeEach(async () => {
   await Blog.insertMany(helper.initialBlogs)
 })
 
-test('all blogs are returned', async () => {
+test('all blogs are returned with same amount of blogs', async () => {
   const response = await api.get('/api/blogs')
 
   assert.strictEqual(response.body.length, helper.initialBlogs.length)
+})
+
+test('blogs are returned with id field (not _id)', async () => {
+  const result = await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  const blogs = result.body
+
+  blogs.forEach(blog => {
+    assert.ok(Object.hasOwn(blog, 'id'))
+    assert.equal(blog._id, undefined)
+  })
 })
 
 after(async () => {
