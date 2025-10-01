40ca2198e076cb6b82cbfcfff488a4a9fd2a951f
Parham-F <parham.parvizfarzam@gmail.com>
2025-10-01 01:27:15 -0600

    4.5*: Helper Functions and Unit Tests, step 3

--- Part4/blogList/utils/list_helper.js
+++ Part4/blogList/utils/list_helper.js
@@ -7,7 +7,13 @@ const totalLikes = (blogs) => {
     return likesList.reduce((sum, item) => sum + item, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  const likesList = blogs.map(blog => blog.likes)
+  return blogs[likesList.indexOf(Math.max(...likesList))]
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
