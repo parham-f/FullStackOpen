e9dbe324a41c0a3ae4aded4b057fe2dcc42ef13a
Parham-F <parham.parvizfarzam@gmail.com>
2025-10-01 16:56:56 -0600

    4.10: Blog List Tests, step 3

--- Part4/blogList/tests/blog_api.test.js
+++ Part4/blogList/tests/blog_api.test.js
@@ -33,6 +33,28 @@ test('blogs are returned with id field (not _id)', async () => {
   })
 })
 
+test('a valid blog can be added ', async () => {
+  const newBlog = {
+    title: "New Blog",
+    author: "New Blog Writer",
+    url: "https://newblog.com/",
+    likes: 77
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
+
+  const contents = blogsAtEnd.map((blog) => blog.content)
+  const {id, ...savedBlogWithoutID} = blogsAtEnd[helper.initialBlogs.length]
+  assert.deepStrictEqual(savedBlogWithoutID, newBlog)
+})
+
 after(async () => {
   await mongoose.connection.close()
 })
