5ee7c71f00e3f7f9507e281ba06eec1fc4831c08
Parham-F <parham.parvizfarzam@gmail.com>
2025-10-02 14:15:28 -0600

    4.15: Blog List Expansion, step 3

---
+++ Part4/blogList/controllers/users.js
@@ -0,0 +1,26 @@
+const bcrypt = require('bcrypt')
+const usersRouter = require('express').Router()
+const User = require('../models/user')
+
+usersRouter.get('/', async (request, response) => {
+  const users = await User.find({})
+  response.json(users)
+})
+
+usersRouter.post('/', async (request, response) => {
+  const { username, name, password } = request.body
+
+  const passwordHash = await bcrypt.hash(password, 10)
+
+  const user = new User({
+    username,
+    name,
+    passwordHash,
+  })
+
+  const savedUser = await user.save()
+
+  response.status(201).json(savedUser)
+})
+
+module.exports = usersRouter
