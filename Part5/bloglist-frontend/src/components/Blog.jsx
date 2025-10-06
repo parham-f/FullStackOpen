a0ba53a6bb8f13d18795f9910ab9f27f0471db56
Parham-F <parham.parvizfarzam@gmail.com>
2025-10-06 15:01:16 -0600

    5.7 Blog List Frontend, step 7

--- Part5/bloglist-frontend/src/components/Blog.jsx
+++ Part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,45 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} - {blog.author}
+import {useState} from 'react'
+
+const Blog = ({blog}) => {
+  const [detailed, setDetailed] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  console.log(blog)
+
+  const toggleDetail = (event) => {
+    setDetailed(!detailed)
+  }
+
+  const handleLike = () => {
+
+  }
+
+  return (
+  <div style={blogStyle}>
+    {!detailed && (
+    <div>
+      {blog.title} - {blog.author}
+      <button onClick={toggleDetail}>View</button>
+    </div>
+    )}
+    {detailed && (
+    <div>
+      {blog.title} - {blog.author}
+      <button onClick={toggleDetail}>Hide</button><br></br>
+      {blog.url}<br></br>
+      Likes: {blog.likes}
+      <button onClick={handleLike}>Like</button><br></br>
+      {blog.user.name}
+    </div>
+    )}
   </div>  
-)
+  )
+}
 
 export default Blog
