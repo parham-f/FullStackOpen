990f1191520b98daf04b9e4487387d2cd39797d8
Parham-F <parham.parvizfarzam@gmail.com>
2025-10-06 18:35:52 -0600

    5.14: Blog List Tests, step 2

--- Part5/bloglist-frontend/src/components/Blog.test.jsx
+++ Part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 test('Display only title and author', () => {
@@ -15,4 +16,25 @@ test('Display only title and author', () => {
     expect(url).not.toBe()
     const likes = screen.queryByText('Likes')
     expect(likes).not.toBe()
+})
+
+test('Display URL and Likes when pressing View button', async () => {
+    const blog = {
+        title: 'test title',
+        author: 'test author',
+        url: 'test URL',
+        user: {
+            username: 'admin',
+            name: 'superUser'
+        }
+    }
+
+    render(<Blog blog={blog}/>)
+
+    const user = userEvent.setup()
+    const button = screen.getByText('View')
+    await user.click(button)
+
+    expect(screen.findByText('test URL')).toBeDefined()
+    expect(screen.findByText('Likes: 1')).toBeDefined()
 })
