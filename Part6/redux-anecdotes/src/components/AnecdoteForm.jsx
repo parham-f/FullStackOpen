1c054fba5da9c583a2beb55c184f726c689ad086
Parham-F <parham.parvizfarzam@gmail.com>
2025-10-09 00:39:48 -0600

    6.11/6.12/6.13 Better Anecdotes, step 9/10/11

--- Part6/redux-anecdotes/src/components/AnecdoteForm.jsx
+++ Part6/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,5 +1,6 @@
 import {useDispatch} from 'react-redux'
 import {addAnecdote} from '../reducers/anecdoteReducer'
+import {setNotification, removeNotification} from '../reducers/notificationReducer'
 
 const AnecdoteForm = () => {
     const dispatch = useDispatch()
@@ -8,6 +9,10 @@ const AnecdoteForm = () => {
         const content = event.target.anecdote.value
         event.target.anecdote.value = ''
         dispatch(addAnecdote(content))
+        dispatch(setNotification(`You added '${content}'`))
+        setTimeout(() => {
+            dispatch(removeNotification())
+        }, 5000) 
     }
     return (
         <form onSubmit={addNew}>
--- Part6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ Part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -1,5 +1,6 @@
 import {useSelector, useDispatch} from 'react-redux'
 import {addVote} from '../reducers/anecdoteReducer'
+import {setNotification, removeNotification} from '../reducers/notificationReducer'
 
 const AnecdoteList = () => {
     const anecdotes = useSelector(state => state.anecdotes)
@@ -12,8 +13,12 @@ const AnecdoteList = () => {
     const sortedAnecdotes = [...filtered].sort((a, b) => b.votes - a.votes)
     const dispatch = useDispatch()
 
-    const vote = (id) => {
-        dispatch(addVote(id))
+    const vote = (anecdote) => {
+        dispatch(addVote(anecdote.id))
+        dispatch(setNotification(`You voted '${anecdote.content}'`))
+        setTimeout(() => {
+            dispatch(removeNotification())
+        }, 5000) 
     }
     
     return (
@@ -25,7 +30,7 @@ const AnecdoteList = () => {
                 </div>
                 <div>
                     has {anecdote.votes}
-                    <button onClick={() => vote(anecdote.id)}>vote</button>
+                    <button onClick={() => vote(anecdote)}>vote</button>
                 </div>
                 </div>
             )}
