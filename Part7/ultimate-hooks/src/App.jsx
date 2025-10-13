d77dc86dfe86e060931c3d63c204bc83006af7fe
Parham-F <parham.parvizfarzam@gmail.com>
2025-10-13 01:04:21 -0600

    7.9: Automatic Code Formatting

--- Part7/ultimate-hooks/src/App.jsx
+++ Part7/ultimate-hooks/src/App.jsx
@@ -1,14 +1,14 @@
-import { useState, useEffect } from 'react'
-import axios from 'axios'
+import { useState, useEffect } from "react"
+import axios from "axios"
 
 const useField = (type) => {
-  const [value, setValue] = useState('')
+  const [value, setValue] = useState("")
 
   const onChange = (event) => {
     setValue(event.target.value)
   }
 
-  const reset = () => setValue('')
+  const reset = () => setValue("")
 
   return {
     input: { type, value, onChange },
@@ -22,46 +22,41 @@ const useResource = (baseUrl) => {
   const [triggerFetch, setTriggerFetch] = useState(true)
 
   useEffect(() => {
-    axios
-        .get(baseUrl)
-        .then(response => {
-          setResources(response.data)
-        })
+    axios.get(baseUrl).then((response) => {
+      setResources(response.data)
+    })
   }, [triggerFetch])
 
   const create = (resource) => {
-    axios.post(baseUrl, resource)
-      .then(() => {
-        setTriggerFetch(!triggerFetch);
-      })
+    axios.post(baseUrl, resource).then(() => {
+      setTriggerFetch(!triggerFetch)
+    })
   }
 
   const service = {
-    create
+    create,
   }
 
-  return [
-    resources, service
-  ]
+  return [resources, service]
 }
 
 const App = () => {
-  const content = useField('text')
-  const name = useField('text')
-  const number = useField('text')
+  const content = useField("text")
+  const name = useField("text")
+  const number = useField("text")
 
-  const [notes, noteService] = useResource('http://localhost:3005/notes')
-  const [persons, personService] = useResource('http://localhost:3005/persons')
+  const [notes, noteService] = useResource("http://localhost:3005/notes")
+  const [persons, personService] = useResource("http://localhost:3005/persons")
 
   const handleNoteSubmit = (event) => {
     event.preventDefault()
     noteService.create({ content: content.value })
     content.reset()
   }
- 
+
   const handlePersonSubmit = (event) => {
     event.preventDefault()
-    personService.create({ name: name.value, number: number.value})
+    personService.create({ name: name.value, number: number.value })
     name.reset()
     number.reset()
   }
@@ -73,15 +68,21 @@ const App = () => {
         <input {...content.input} />
         <button>create</button>
       </form>
-      {notes.map(n => <p key={n.id}>{n.content}</p>)}
+      {notes.map((n) => (
+        <p key={n.id}>{n.content}</p>
+      ))}
 
       <h2>persons</h2>
       <form onSubmit={handlePersonSubmit}>
-        name <input {...name.input} /> <br/>
+        name <input {...name.input} /> <br />
         number <input {...number.input} />
         <button>create</button>
       </form>
-      {persons.map(n => <p key={n.id}>{n.name} {n.number}</p>)}
+      {persons.map((n) => (
+        <p key={n.id}>
+          {n.name} {n.number}
+        </p>
+      ))}
     </div>
   )
 }
